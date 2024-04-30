b6e0435c7281cf5a58175d00ca29fd6f551df099
Evan Schoffstall <evan.schoffstall@gmail.com>
2024-04-30 15:01:12 -0400

    refactor for cleanliness

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 #[macro_use]
 extern crate serde_derive;
 
-use clap::Arg;
-use std::collections::BTreeMap;
-use std::fs::File;
-use std::io::prelude::*;
+use clap::{crate_authors, crate_name, crate_version, Arg, Command};
+use std::{
+    collections::BTreeMap,
+    error::Error,
+    fs::File,
+    io::{self, prelude::*},
+    process::Command as ProcessCommand,
+};
 
 macro_rules! die(
-  ($($arg:tt)*) => { {
-    writeln!(std::io::stderr(), $($arg)*)
-      .expect("Failed to print to stderr");
-    std::process::exit(1);
-  } }
+    ($($arg:tt)*) => { {
+        writeln!(io::stderr(), $($arg)*).expect("Failed to print to stderr");
+        std::process::exit(1);
+    } }
 );
 
 #[derive(Deserialize)]
@@ -20,41 +23,35 @@ struct Config {
     user_commands: Option<BTreeMap<String, BTreeMap<String, String>>>,
 }
 
-fn read_config<P: AsRef<std::path::Path>>(path: P) -> Result<Config, Box<dyn std::error::Error>> {
+fn read_config<P: AsRef<std::path::Path>>(path: P) -> Result<Config, Box<dyn Error>> {
     let mut contents = String::new();
-
     File::open(path)?.read_to_string(&mut contents)?;
-
-    let config: Config = toml::from_str(&contents)?;
-    Ok(config)
+    toml::from_str(&contents).map_err(|e| e.into())
 }
 
-fn run_command(command: &str, args: &str) -> Result<i32, Box<dyn std::error::Error>> {
-    let mut formatted_command = command.to_owned();
+fn run_command(command: &str, args: &str) -> Result<i32, Box<dyn Error>> {
+    let formatted_command = args
+        .split_whitespace()
+        .enumerate()
+        .fold(command.replace("%@", args), |cmd, (i, arg)| {
+            cmd.replace(&format!("%{}", i + 1), arg)
+        });
 
-    let args_vec: Vec<&str> = args.split_whitespace().collect();
-
-    for (i, arg) in args_vec.iter().enumerate() {
-        let placeholder = format!("%{}", i + 1);
-        formatted_command = formatted_command.replace(&placeholder, arg);
-    }
-    formatted_command = formatted_command.replace("%@", args);
-
-    let mut child = std::process::Command::new("/bin/sh")
+    ProcessCommand::new("/bin/sh")
         .arg("-c")
         .arg(&formatted_command)
-        .spawn()?;
-
-    child
+        .spawn()?
         .wait()?
         .code()
-        .ok_or_else(|| std::io::Error::last_os_error().into())
+        .ok_or_else(|| {
+            std::io::Error::new(std::io::ErrorKind::Other, "No exit code available").into()
+        })
 }
 
 fn main() {
-    let matches = clap::Command::new(clap::crate_name!())
-        .version(clap::crate_version!())
-        .author(clap::crate_authors!())
+    let matches = Command::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
         .about("resh is a restricted shell that only allows whitelisted commands")
         .arg(
             Arg::new("command")
@@ -64,41 +61,37 @@ fn main() {
         )
         .get_matches();
 
-    let command_alias_and_args = match std::env::var("SSH_ORIGINAL_COMMAND") {
-        Ok(cmd) => cmd,
-        Err(_) => matches
-            .get_one::<String>("command")
-            .cloned()
-            .unwrap_or_else(|| die!("Usage: {} <command alias> <arguments>", clap::crate_name!())),
-    };
+    let command_alias_and_args = std::env::var("SSH_ORIGINAL_COMMAND")
+        .or_else(|_| {
+            matches
+                .get_one::<String>("command")
+                .cloned()
+                .ok_or_else(|| "Command not found")
+        })
+        .unwrap_or_else(|_| die!("Usage: {} <command alias> <arguments>", crate_name!()));
 
-    let command_args: Vec<&str> = command_alias_and_args.split_whitespace().collect();
-
-    let (command_alias, command_args) = match command_args.split_at(1) {
-        (c, a) if !c.is_empty() => (c[0].to_string(), a.join(" ")),
-        _ => die!("Usage: {} <command alias> <arguments>", clap::crate_name!()),
-    };
+    let mut command_args = command_alias_and_args.split_whitespace();
+    let command_alias = command_args
+        .next()
+        .unwrap_or_else(|| die!("Usage: {} <command alias> <arguments>", crate_name!()));
 
     let config_file = std::env::var("RESH_CONFIG").unwrap_or_else(|_| "/etc/resh.toml".to_string());
-
-    let config: Config = read_config(&config_file).unwrap_or_else(|e| {
-        die!("Failed to read {}: {}", config_file, e);
-    });
+    let config =
+        read_config(&config_file).unwrap_or_else(|e| die!("Failed to read {}: {}", config_file, e));
 
     let username = std::env::var("USER").unwrap_or_else(|_| "default".to_string());
-
     let command = config
         .user_commands
         .as_ref()
         .and_then(|user_cmds| {
             user_cmds
                 .get(&username)
-                .and_then(|cmds| cmds.get(&command_alias))
+                .and_then(|cmds| cmds.get(command_alias))
         })
-        .or_else(|| config.commands.get(&command_alias))
+        .or_else(|| config.commands.get(command_alias))
         .unwrap_or_else(|| die!("Undefined command alias: {}", command_alias));
 
-    let exitcode = run_command(command, &command_args).unwrap_or(1);
-
+    let exitcode =
+        run_command(command, &command_args.collect::<Vec<&str>>().join(" ")).unwrap_or(1);
     std::process::exit(exitcode);
 }
