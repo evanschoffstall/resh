d34d38a98ebf9a3456a44fe29381f2ece2f6e51e
Evan Schoffstall <evan.schoffstall@gmail.com>
2024-04-30 14:43:29 -0400

    add back in handling for SSH_ORIGINAL_COMMAND

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate serde_derive;
 
-use clap::{Arg, Command};
+use clap::Arg;
 use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::prelude::*;
@@ -29,10 +29,20 @@ fn read_config<P: AsRef<std::path::Path>>(path: P) -> Result<Config, Box<dyn std
     Ok(config)
 }
 
-fn run_command(command: &str) -> Result<i32, Box<dyn std::error::Error>> {
+fn run_command(command: &str, args: &str) -> Result<i32, Box<dyn std::error::Error>> {
+    let mut formatted_command = command.to_owned();
+
+    let args_vec: Vec<&str> = args.split_whitespace().collect();
+
+    for (i, arg) in args_vec.iter().enumerate() {
+        let placeholder = format!("%{}", i + 1);
+        formatted_command = formatted_command.replace(&placeholder, arg);
+    }
+    formatted_command = formatted_command.replace("%@", args);
+
     let mut child = std::process::Command::new("/bin/sh")
         .arg("-c")
-        .arg(command)
+        .arg(&formatted_command)
         .spawn()?;
 
     child
@@ -42,10 +52,10 @@ fn run_command(command: &str) -> Result<i32, Box<dyn std::error::Error>> {
 }
 
 fn main() {
-    let matches = Command::new(clap::crate_name!())
+    let matches = clap::Command::new(clap::crate_name!())
         .version(clap::crate_version!())
         .author(clap::crate_authors!())
-        .about("resh is a restricted (ssh) shell that only allows whitelisted commands")
+        .about("resh is a restricted shell that only allows whitelisted commands")
         .arg(
             Arg::new("command")
                 .short('c')
@@ -54,13 +64,19 @@ fn main() {
         )
         .get_matches();
 
-    let command_alias = match matches.get_one::<String>("command") {
-        Some(cmd) => cmd.clone(),
+    let command_alias_and_args = match std::env::var("SSH_ORIGINAL_COMMAND") {
+        Ok(cmd) => cmd,
+        Err(_) => matches
+            .get_one::<String>("command")
+            .cloned()
+            .unwrap_or_else(|| die!("Usage: {} <command alias> <arguments>", clap::crate_name!())),
+    };
 
-        None => match std::env::var("SSH_ORIGINAL_COMMAND") {
-            Ok(cmd) => cmd,
-            _ => die!("Usage: {} <command alias>", clap::crate_name!()),
-        },
+    let command_args: Vec<&str> = command_alias_and_args.split_whitespace().collect();
+
+    let (command_alias, command_args) = match command_args.split_at(1) {
+        (c, a) if !c.is_empty() => (c[0].to_string(), a.join(" ")),
+        _ => die!("Usage: {} <command alias> <arguments>", clap::crate_name!()),
     };
 
     let config_file = std::env::var("RESH_CONFIG").unwrap_or_else(|_| "/etc/resh.toml".to_string());
@@ -82,7 +98,7 @@ fn main() {
         None => die!("Undefined command alias: {}", command_alias),
     };
 
-    let exitcode = run_command(full_command).unwrap_or(1);
+    let exitcode = run_command(full_command, &command_args).unwrap_or(1);
 
     std::process::exit(exitcode);
 }
