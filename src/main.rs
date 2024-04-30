f9c212910e84f7d898ddd89020788dbeadf392d3
Evan Schoffstall <evan.schoffstall@gmail.com>
2024-04-30 13:54:36 -0400

    implement user-specific whitelist

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 #[macro_use]
 extern crate serde_derive;
 
+use clap::{Arg, Command};
 use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::prelude::*;
 
-use clap::{App, Arg};
-
 macro_rules! die(
-    ($($arg:tt)*) => { {
-        writeln!(std::io::stderr(), $($arg)*)
-            .expect("Failed to print to stderr");
-        std::process::exit(1);
-    } }
+  ($($arg:tt)*) => { {
+    writeln!(std::io::stderr(), $($arg)*)
+      .expect("Failed to print to stderr");
+    std::process::exit(1);
+  } }
 );
 
 #[derive(Deserialize)]
 struct Config {
     commands: BTreeMap<String, String>,
+    user_commands: Option<BTreeMap<String, BTreeMap<String, String>>>,
 }
 
 fn read_config<P: AsRef<std::path::Path>>(path: P) -> Result<Config, Box<dyn std::error::Error>> {
@@ -42,20 +42,21 @@ fn run_command(command: &str) -> Result<i32, Box<dyn std::error::Error>> {
 }
 
 fn main() {
-    let matches = App::new(clap::crate_name!())
+    let matches = Command::new(clap::crate_name!())
         .version(clap::crate_version!())
         .author(clap::crate_authors!())
         .about("resh is a restricted (ssh) shell that only allows whitelisted commands")
         .arg(
-            Arg::with_name("command")
+            Arg::new("command")
                 .short('c')
                 .help("Alias of command to execute")
                 .value_name("COMMAND"),
         )
         .get_matches();
 
-    let command_alias = match matches.value_of("command") {
-        Some(cmd) => String::from(cmd),
+    let command_alias = match matches.get_one::<String>("command") {
+        Some(cmd) => cmd.clone(),
+
         None => match std::env::var("SSH_ORIGINAL_COMMAND") {
             Ok(cmd) => cmd,
             _ => die!("Usage: {} <command alias>", clap::crate_name!()),
@@ -68,7 +69,15 @@ fn main() {
         die!("Failed to read {}: {}", config_file, e);
     });
 
-    let full_command = match config.commands.get(&command_alias) {
+    let username = std::env::var("USER").unwrap_or_else(|_| "default".to_string());
+
+    let commands = config
+        .user_commands
+        .as_ref()
+        .and_then(|user_cmds| user_cmds.get(&username))
+        .unwrap_or_else(|| &config.commands);
+
+    let full_command = match commands.get(&command_alias) {
         Some(cmd) => cmd,
         None => die!("Undefined command alias: {}", command_alias),
     };
